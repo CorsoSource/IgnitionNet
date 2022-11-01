0368612bab5a16e0aae5fceb0b65ed9ebe8b2c7f
William Bohn <william.bohn@corsosystems.com>
2022-11-01 00:37:49 -0500

    Patch DbContext extensions

---
+++ IgnitionNet/History/HistoryContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using CorsoSystems.IgnitionNet.History.Db;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorsoSystems.IgnitionNet.History
+{
+    public static class HistoryContextExtensions
+    {
+        public static IDbManager GetDbManager(this DbContext context)
+        {
+            if (context.Database.IsNpgsql())
+            {
+                return new NpgsqlManager(context);
+            }
+
+            throw new NotImplementedException($"{context.Database.ProviderName} is not a supported provider.");
+        }
+
+        public static void PatchDatabase(this DbContext context)
+        {
+            GetDbManager(context).PatchDatabase();
+        }
+
+        public static void PatchDatabase(this DbContext context, ICollection<string> partitionNames)
+        {
+            GetDbManager(context).PatchDatabase(partitionNames);
+        }
+    }
+}
